34010a0c0cc0b77509b94170e2257b84ffac520d
Tonb73 <Tonby73@yandex.ru>
2024-11-06 11:07:14 +0400

    Добавил javaDoc и описал класс Gun

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         boolean res = registration("ASD_123", "123_F","123_F");
         System.out.println(res);
-        BoltActionGun boltActionRifle = new BoltActionGun("Российская империя","M1891",5,true,true);
+        BoltActionGun boltActionRifle =
+                new BoltActionGun("Российская империя","M1891",5,true,true);
 
         boltActionRifle.reload();
         boltActionRifle.shoot();
@@ -16,7 +17,8 @@ public class Main {
         boltActionRifle.reload();
         boltActionRifle.showGunInfo();
 
-        MachineGunGun machineGun = new MachineGunGun("США", "BAR",30,100);
+        MachineGunGun machineGun =
+                new MachineGunGun("США", "BAR",30,100);
         machineGun.shoot();
         machineGun.reload();
         machineGun.shoot();
@@ -34,15 +36,22 @@ public class Main {
     }
     public static boolean registration(String login, String password, String confirmPassword){
         try{
-            if(login == null){throw new WrongLoginException("Логин не введен");}
-            if(login.length() >= 20){throw new WrongLoginException("Логин должен содержать менее 20 символов");}
-            if(!login.matches("[A-Za-z0-9_]+")){throw new WrongLoginException("Логин содержит недопустимый символ");}
+            if(login == null)
+            {throw new WrongLoginException("Логин не введен");}
+            if(login.length() >= 20)
+            {throw new WrongLoginException("Логин должен содержать менее 20 символов");}
+            if(!login.matches("[A-Za-z0-9_]+"))
+            {throw new WrongLoginException("Логин содержит недопустимый символ");}
 
-            if(password == null){throw new WrongPasswordException("Пароль не введен");}
-            if(password.length() >= 20){throw new WrongPasswordException("Пароль должен содержать менее 20 символов");}
-            if(!password.matches("[A-Za-z0-9_]+")){throw new WrongPasswordException("Пароль содержит недопустимый символ");}
+            if(password == null)
+            {throw new WrongPasswordException("Пароль не введен");}
+            if(password.length() >= 20)
+            {throw new WrongPasswordException("Пароль должен содержать менее 20 символов");}
+            if(!password.matches("[A-Za-z0-9_]+"))
+            {throw new WrongPasswordException("Пароль содержит недопустимый символ");}
 
-            if(!password.equals(confirmPassword)){throw new WrongPasswordException("Пароли не совпадают");}
+            if(!password.equals(confirmPassword))
+            {throw new WrongPasswordException("Пароли не совпадают");}
 
         }catch (WrongLoginException | WrongPasswordException e){
             System.out.println(e.getMessage());
